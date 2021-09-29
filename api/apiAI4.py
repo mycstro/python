ea949c1974d0909694ab27eaef629f2eb40ed45a
Michael Rouse <michael.mcrtech@gmail.com>
2021-09-29 18:04:06 -0400

    first commit

---
+++ api/apiAI4.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api, reqparse, abort, fields, marshal_with
+import pandas as pd
+import ast
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+data_fields = {
+    'id': fields.Integer
+}
+
+class Data(Resource):
+    def get(self):        
+        with open('data/data.json', 'rb') as f:
+            data = json.load(f)
+            if not data:
+                abort(400, message="Could not find any data.")
+        return data
+
+    def post(self):
+        data = request.get_json()
+        app.logger.info("Applying post.")
+        with open('data/data.json', 'w') as outfile:
+            json.dump(data, outfile)
+            post = "Posted data: {0}".format(data)
+        return jsonify(post)
+
+api.add_resource(Data, '/data')
+
+@app.route('/', methods=['GET'])
+def index():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.run(debug=True)
